19eb1def1d2ed9f7c3b5c00879f8521c0da9a316
William Gallagher <wgallagher3@gmail.com>
2017-08-07 18:31:21 -0500

    more fun

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-function callFunction(fun) {
-    fun();
-};
+var counter = require('./count');
 
-//function expression
-var sayBye = function() {
-    console.log('bye');
-};
-
-callFunction(sayBye);
+console.log(counter(['shaun', 'crystal', 'ryu']));
