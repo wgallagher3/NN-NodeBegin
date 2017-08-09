974fd70f33c343eb3004eddc47f26b0821b35651
William Gallagher <wgallagher3@gmail.com>
2017-08-08 22:04:58 -0500

    todays fun

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
     console.log('request was made: ' + req.url);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hey ninjas');
+    res.end('feed me popcorn');
+
 });
 
 server.listen(3000, '127.0.0.1');
